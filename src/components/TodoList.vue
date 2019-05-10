a5c54aded275b0853380b111a1bcbce46b90b030
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-10 19:16:27 +0900

    仮表示までできた

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,35 @@
 <template>
-    <div>TodoList</div>
+    <div>
+        <div>TodoList</div>
+        <div v-for="item in items" :key="item.id">
+            <div class="id">{{item.id}}</div>
+            <div class="name">タスク名：{{ item.name }}</div>
+        </div>
+    </div>
+    
 </template>
 
 <script>
 export default {
-    name: "TodoList"
+    name: "TodoList",
+    data() {
+        return {
+            // 仮配列にあわせて適当な値から
+            count: 3,
+            items: [
+                {
+                    id: 1,
+                    name: "TODOリストを作る!",
+                    finish: false
+                },
+                {
+                    id: 2,
+                    name: "公開する",
+                    finish: false
+                }
+            ]
+        }
+    }
 };
 </script>
 
