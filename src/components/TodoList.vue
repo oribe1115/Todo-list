4cdf2c7c0d78c1cafd170f1d53dddc6a338beb19
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-16 18:20:52 +0900

    pritterさんにフォーマットしてもらった

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,171 +1,176 @@
 <template>
-    <div class="body">
-        <div class="title">TodoList</div>
-        
+  <div class="body">
+    <div class="title">TodoList</div>
 
-        <div class="main">
-
-            <div class="task unfinished">
-                <div class="category-message">Let's begin!</div>
-                <transition-group name="all-tasks">
-                    <div v-for="item in sortWithImportance" :key="item.id">
-                        <transition name="show-item">
-                            <div v-if="!(item.finish)">
-                                <div class="item">
-                                    <div class="check">
-                                        <div v-bind:class="{checked: item.finish, unchecked: item.finish == false}">
-                                            <div @click="changeStatus(item)">
-                                                <font-awesome-icon icon="check" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="name">{{ item.name }}</div>
-                                    <div class="star-box">
-                                        <div v-for="i in 5" :key="i">
-                                            <div v-bind:class="{filled: i <= compareForFillStars(item), blank: i > compareForFillStars(item)}">
-                                                <div @click="changeImportance(i, item)" @mouseover="mouseOver(i, item)" @mouseleave="mouseLeave(item)">
-                                                    <font-awesome-icon icon="star" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </transition>
+    <div class="main">
+      <div class="task unfinished">
+        <div class="category-message">Let's begin!</div>
+        <transition-group name="all-tasks">
+          <div v-for="item in sortWithImportance" :key="item.id">
+            <transition name="show-item">
+              <div v-if="!(item.finish)">
+                <div class="item">
+                  <div class="check">
+                    <div v-bind:class="{checked: item.finish, unchecked: item.finish == false}">
+                      <div @click="changeStatus(item)">
+                        <font-awesome-icon icon="check"/>
+                      </div>
                     </div>
-                </transition-group>
-            </div>  
-
-            <div class="for-change">
-                <div class="category-message">New Task</div>
-                <div class="buttons">
-                    <div class="addition">
-                        <label>
-                            <input type="text" v-model="newItemName">
-                        </label>
-                        <button @click="addItem">add</button>
+                  </div>
+                  <div class="name">{{ item.name }}</div>
+                  <div class="star-box">
+                    <div v-for="i in 5" :key="i">
+                      <div
+                        v-bind:class="{filled: i <= compareForFillStars(item), blank: i > compareForFillStars(item)}"
+                      >
+                        <div
+                          @click="changeImportance(i, item)"
+                          @mouseover="mouseOver(i, item)"
+                          @mouseleave="mouseLeave(item)"
+                        >
+                          <font-awesome-icon icon="star"/>
+                        </div>
+                      </div>
                     </div>
+                  </div>
                 </div>
-            </div>
+              </div>
+            </transition>
+          </div>
+        </transition-group>
+      </div>
 
-            <div class="task finished">
-                <div class="category-message">Finished</div>
-                <transition-group name="all-tasks">
-                    <div v-for="item in sortWithImportance" :key="item.id">
-                        <transition name="show-item">
-                            <div v-if="item.finish">
-                                <div class="item">
-                                    <div class="check">
-                                        <div v-bind:class="{checked: item.finish, unchecked: item.finish == false}">
-                                            <div @click="changeStatus(item)">
-                                                <font-awesome-icon icon="check" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="name">{{ item.name }}</div>
-                                    <div class="star-box">
-                                        <div v-for="i in 5" :key="i">
-                                            <div v-bind:class="{filled: i <= compareForFillStars(item), blank: i > compareForFillStars(item)}">
-                                                <div @click="changeImportance(i, item)" @mouseover="mouseOver(i, item)" @mouseleave="mouseLeave(item)">
-                                                    <font-awesome-icon icon="star" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </transition>
-                    </div>
-                </transition-group>
-            </div>
-            
-
-            
-
+      <div class="for-change">
+        <div class="category-message">New Task</div>
+        <div class="buttons">
+          <div class="addition">
+            <label>
+              <input type="text" v-model="newItemName">
+            </label>
+            <button @click="addItem">add</button>
+          </div>
         </div>
+      </div>
+
+      <div class="task finished">
+        <div class="category-message">Finished</div>
+        <transition-group name="all-tasks">
+          <div v-for="item in sortWithImportance" :key="item.id">
+            <transition name="show-item">
+              <div v-if="item.finish">
+                <div class="item">
+                  <div class="check">
+                    <div v-bind:class="{checked: item.finish, unchecked: item.finish == false}">
+                      <div @click="changeStatus(item)">
+                        <font-awesome-icon icon="check"/>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="name">{{ item.name }}</div>
+                  <div class="star-box">
+                    <div v-for="i in 5" :key="i">
+                      <div
+                        v-bind:class="{filled: i <= compareForFillStars(item), blank: i > compareForFillStars(item)}"
+                      >
+                        <div
+                          @click="changeImportance(i, item)"
+                          @mouseover="mouseOver(i, item)"
+                          @mouseleave="mouseLeave(item)"
+                        >
+                          <font-awesome-icon icon="star"/>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </transition>
+          </div>
+        </transition-group>
+      </div>
     </div>
-    
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-    name: "TodoList",
-    data() {
-        return {
-            count: 1,
-            newItemName: "",
-            items: [],
-            serverLink: "https://to-hutohu.trap.show/naro-todo-server/oribe/tasks"
+  name: "TodoList",
+  data() {
+    return {
+      count: 1,
+      newItemName: "",
+      items: [],
+      serverLink: "https://to-hutohu.trap.show/naro-todo-server/oribe/tasks"
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.newItemName != "") {
+        let newId = 1;
+        for (const item of this.items) {
+          if (newId == item.id) {
+            newId += 1;
+          } else {
+            break;
+          }
         }
-    },
-    methods: {
-        addItem() {
-            if(this.newItemName != ""){
-                let newId = 1;
-                for (const item of this.items) {
-                    if (newId == item.id) {
-                        newId += 1;
-                    } else {
-                        break;
-                    }
-                }
 
-                const item = {
-                    id: newId,
-                    name: this.newItemName,
-                    finish: false,
-                    importance: 1,
-                    hoverStar: 0
-                };
-                this.newItemName = "";
-                this.postToServer(item);
-            }
-        },
-        changeImportance(i, item) {
-            item.importance = i;
-            axios.put(this.serverLink + "/" + item.id, item);
-        },
-        mouseOver(i, item) {
-            item.hoverStar = i;
-        },
-        mouseLeave(item) {
-            item.hoverStar = 0;
-        },
-        compareForFillStars(item) {
-            if(item.hoverStar != 0){
-                return item.hoverStar;
-            } else {
-                return item.importance;
-            }
-        },
-        changeStatus(item){
-            if (item.finish == true){
-                item.finish = false;
-                axios.put(this.serverLink + "/" + item.id, item);
-            } else {
-                item.finish = true;
-                axios.put(this.serverLink + "/" + item.id, item);
-            }
-        },
-        postToServer(item) {
-            axios.post(this.serverLink, item).then(response => (this.items = response.data));
-        },
-        getFromServer() {
-            axios.get(this.serverLink).then(response => (this.items = response.data));
-        }
+        const item = {
+          id: newId,
+          name: this.newItemName,
+          finish: false,
+          importance: 1,
+          hoverStar: 0
+        };
+        this.newItemName = "";
+        this.postToServer(item);
+      }
     },
-    computed: {
-        sortWithImportance(){
-            return [...this.items].sort((a, b) => b.importance - a.importance);
-        }
+    changeImportance(i, item) {
+      item.importance = i;
+      axios.put(this.serverLink + "/" + item.id, item);
     },
-    mounted() {
-        axios
-            .get(this.serverLink)
-            .then(response => (this.items = response.data))
+    mouseOver(i, item) {
+      item.hoverStar = i;
+    },
+    mouseLeave(item) {
+      item.hoverStar = 0;
+    },
+    compareForFillStars(item) {
+      if (item.hoverStar != 0) {
+        return item.hoverStar;
+      } else {
+        return item.importance;
+      }
+    },
+    changeStatus(item) {
+      if (item.finish == true) {
+        item.finish = false;
+        axios.put(this.serverLink + "/" + item.id, item);
+      } else {
+        item.finish = true;
+        axios.put(this.serverLink + "/" + item.id, item);
+      }
+    },
+    postToServer(item) {
+      axios
+        .post(this.serverLink, item)
+        .then(response => (this.items = response.data));
+    },
+    getFromServer() {
+      axios.get(this.serverLink).then(response => (this.items = response.data));
     }
+  },
+  computed: {
+    sortWithImportance() {
+      return [...this.items].sort((a, b) => b.importance - a.importance);
+    }
+  },
+  mounted() {
+    axios.get(this.serverLink).then(response => (this.items = response.data));
+  }
 };
 </script>
 
